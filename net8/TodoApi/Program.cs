996cdeb5cda5115191f8653dc2d5fc581f732ea8
Dave Amiana <amianism25@gmail.com>
2024-07-29 09:51:27 +0800

    sqlite db connector and documentation for building the dotnet webapi

--- net8/TodoApi/Program.cs
+++ net8/TodoApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseSqlite("Data Source=TodoList.db"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
